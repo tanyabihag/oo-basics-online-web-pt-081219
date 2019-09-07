1c253dce78f82802ae524549e915e37c08736892
Tanya Bihag <tbihag@gmail.com>
2019-09-07 22:31:36 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,9 +7,16 @@ class Shoe
   end
     
   def cobble
+<<<<<<< HEAD
     self.condition = "new"
     puts "Your shoe is as good as new!"
   end
   
+=======
+    puts "Your shoe is as good as new!"
+  
+  end
+    shoe.condition = "new"
+>>>>>>> 3077a309391668cba37b986d24613431c8268cc0
 end
 
